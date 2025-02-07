d525aa95ed484fcfd48c20c086bece014b422154
Peter Ndungu <93506710+petreleven@users.noreply.github.com>
2025-02-07 18:50:29 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,12 @@
-from flask import Flask
-import cv2
+from flask import Flask, redirect, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def server():
-    return "hello"
+@app.before_request
+def before_request():
+    if not request.is_secure:
+        url = request.url.replace('http://', 'https://', 1)
+        return redirect(url, code=301)
 
 
 #app.run(debug=True, port="5000", host="0.0.0.0")
